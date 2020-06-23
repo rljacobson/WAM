e66f87ed100eaa33c49ae21124e006393c1ae02d
Robert Jacobson <rljacobson@gmail.com>
2020-06-23 07:06:30 -0400

    Folded binary.rs into instruction.rs.

--- src/bytecode/instruction.rs
+++ src/bytecode/instruction.rs
@@ -1,5 +1,9 @@
+/*!
+  Representation of VM instructions abstractly and as binary bytecode.
+*/
 
-// use std::convert::TryFrom;
+
+use std::convert::TryFrom;
 use std::fmt::{Display, Formatter};
 
 use strum_macros::{Display as StrumDisplay, IntoStaticStr, EnumString};
@@ -7,7 +11,41 @@ use num_enum::{TryFromPrimitive, IntoPrimitive};
 
 use crate::address::Address;
 use crate::functor::Functor;
-use crate::bytecode::Word;
+
+
+// If you change this you must also change the encode and decode methods of everything that
+// serializes to Words.
+pub type Word = u32;
+
+/*
+  Convenience for decomposing a u64 into a high word and a low word. In
+  fact, the bytecode is designed so that no bit operations have to span
+  a 32-bit word boundary, so this is more convenient than a u64 in general.
+*/
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub struct DoubleWord {
+  pub low  : Word,
+  pub high : Word
+}
+
+/// An `Either` type for an encoded instruction, allowing the instruction to be
+/// either one word or two.
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub enum Bytecode {
+  Word(Word),
+  DoubleWord(DoubleWord)
+}
+
+
+impl Display for Bytecode{
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    match self{
+      Bytecode::Word(word)         => write!(f, "0x{:0>4X}", word),
+      Bytecode::DoubleWord(double) => write!(f, "0x{:0>5X}{:0>4X}", double.high, double.low)
+    }
+
+  }
+}
 
 /**
   Opcodes of the virtual machine.
@@ -157,3 +195,146 @@ impl Display for Instruction {
     }
   }
 }
+
+
+
+pub fn try_decode_instruction(encoded: &Bytecode) -> Option<Instruction> {
+  let mut bytecode = DoubleWord { low: 0, high: 0 };
+  match &encoded {
+    Bytecode::Word(w)        => { bytecode.low = *w; }
+    Bytecode::DoubleWord(tw) => { bytecode = *tw; }
+  }
+
+  let opcode: Operation =
+  match Operation::try_from((bytecode.low & 0xFF) as u8) {
+    Ok(oc)  => oc,
+    Err(_e) => { return None; }
+  };
+
+  let instruction = // = the value of the following giant if statement:
+  if opcode.code() < MAX_FUNCTOR_OPCODE {
+    // [OpCode:8][Address:24][Name:16][Arity:16]
+    let address = match Address::try_decode(bytecode.low >> 8){
+      Some(a) => a,
+      None    => { return None; }
+    };
+    Instruction::Binary {
+      opcode,
+      address,
+      argument: Argument::Functor(Functor::dec(bytecode.high)),
+    }
+  }
+
+  else if opcode.code() < MAX_DOUBLE_WORD_OPCODE {
+    // [OpCode:8][Address:24][Address:24][Reserved:8]
+    let address = match Address::try_decode(bytecode.low >> 8){
+      Some(a) => a,
+      None    => { return None; }
+    };
+    let address2 = match Address::try_decode(bytecode.high){
+      Some(a) => a,
+      None    => { return None; }
+    };
+    Instruction::Binary {
+      opcode,
+      address,
+      argument: Argument::Address(address2)
+    }
+  }
+
+  else if opcode.code() < MAX_UNARY_OPCODE {
+    // [OpCode:8][Address:24]
+    let argument =
+    match opcode {
+
+      Operation::Allocate => Argument::Word(bytecode.low >> 8),
+
+      _                   => {
+        let address = match Address::try_decode(bytecode.low >> 8) {
+          Some(a) => a,
+          None    => { return None; }
+        };
+        Argument::Address(address)
+      }
+
+    };
+    Instruction::Unary {
+      opcode,
+      argument
+    }
+  }
+
+  else {
+    // [OpCode:8]
+    Instruction::Nullary(opcode)
+  };
+
+  Some(instruction)
+}
+
+
+
+/**
+  Encodes the instruction into bytecode. It is the caller's responsibility to
+  use the correct `Instruction` variant for the given opcode.
+*/
+pub fn encode_instruction(instruction: &Instruction) -> Bytecode {
+  match instruction{
+
+    Instruction::Binary {opcode, address, argument} => {
+      let high = match argument {
+        Argument::Address(address2) => address2.enc(),
+        Argument::Functor(functor)  => functor.enc(),
+        Argument::Word(_)           => {
+          unreachable!("Error: `Argument::Word` cannot be an argument to `Instruction::Binary`.");
+        }
+      };
+      Bytecode::DoubleWord(
+        DoubleWord {
+          low: opcode.code() + (address.enc() << 8),
+          high,
+        }
+      )
+    }
+
+    Instruction::Unary {opcode, argument} => {
+      // [OpCode:8][Address:24]
+      let parameter = match argument{
+        Argument::Address(address) => address.enc(),
+        Argument::Word(word)       => *word,
+        Argument::Functor(_)       => {
+          unreachable!(
+            "Error: `Argument::Functor` cannot be an argument to `Instruction::Unary`."
+          );
+        },
+      };
+      Bytecode::Word( opcode.code() + (parameter << 8) )
+    },
+
+    Instruction::Nullary(opcode)   => {
+      // [OpCode:8]
+      Bytecode::Word(opcode.code())
+    },
+  }
+}
+
+
+/// Returns the size in WORDS of an instruction for the corresponding opcode.
+#[allow(dead_code)]
+pub fn instruction_size(opcode: &Operation) -> Word{
+  match opcode.code() < MAX_DOUBLE_WORD_OPCODE {
+    true  => 2, // Two words
+    false => 1  // One Word
+  }
+}
+
+/**
+  Given the low word of an instruction, determines whether the entire instruction is two
+  words. This is how to determine if a second word needs to be provided for decoding.
+
+  Note that this function does not check if the input has a valid opcode.
+*/
+pub fn is_double_word_instruction(word: &Word) -> bool{
+  (*word & 0xFF) < MAX_DOUBLE_WORD_OPCODE
+}
+
--- src/bytecode/mod.rs
+++ src/bytecode/mod.rs
@@ -1,37 +1,36 @@
 /*!
 
-  The VM uses a 32 bit little-endian word size. Instructions are either 32 or 64
-  bits (including opcode, see last paragraph). Memory blocks are 64 bit aligned, and
-  instructions within memory are 32 bit aligned, with trailing padding as necessary.
-  Instructions are decoded bytewise. Memory addresses are 24 bits (3 bytes) and
-  address words, not bytes. The sizes of instruction components are as follows:
+  The VM uses a 32 bit little-endian word size. Instructions are either 32 or 64 bits (including
+  opcode, see last paragraph). Memory blocks are 64 bit aligned, and instructions within memory are
+  32 bit aligned, with trailing padding as necessary. Memory addresses are 24 bits (3 bytes), two
+  bits of which are for the address's tag, and address words, not bytes. The sizes of instruction
+  components are as follows:
 
     Opcode:   8 bits
     Address: 24 bits
     Arity:   24 bits
 
-  Functors do not appear explicitly in the bytecode. They are symbolic representations
-  labeling locations in code memory. During compilation and optionally for execution
-  (for diagnostic or pedagogic purposes),  functors are resolved to their address via
-  a lookup in a symbol table maintained for that purpose.
+  Functors do not appear explicitly in the bytecode. They are symbolic representations labeling
+  locations in code memory. During compilation and optionally for execution (for diagnostic or
+  pedagogic purposes), functors are resolved to their address via a lookup in a symbol table
+  maintained for that purpose.
 
-  One design decision that needed to be made is whether to store the arguments of the
-  instruction as data members of enum variants, with one variant per opcode. Rust enums use
-  an 8 bit discriminant and have size equal to the largest data payload plus the discriminant.
-  (In certain circumstances not relevant here, the compiler can optimize enum values to be
-  smaller.) Since the largest instruction is 48 bits excluding opcode, the equivalent enum values
-  are 56 bits. Even without considering data alignment, this would waste ~50% of allocated
-  memory for instructions on average, which is unacceptably wasteful. Instead, an enum is
-  only used for the opcode itself, not the entire instruction, and inhabits a single byte.
+  One design decision that needed to be made is whether to store the arguments of the instruction as
+  data members of enum variants, with one variant per opcode. Rust enums use an 8 bit discriminant
+  and have size equal to the largest data payload plus the discriminant. (In certain circumstances
+  not relevant here, the compiler can optimize enum values to be smaller.) Since the largest
+  instruction is 48 bits excluding opcode, the equivalent enum values are 56 bits. Even without
+  considering data alignment, this would waste ~50% of allocated memory for instructions on average,
+  which is unacceptably wasteful. Instead, an enum is only used for the opcode itself, not the
+  entire instruction, and inhabits a single byte.
 
 */
 
 pub use assembly::{parse_assembly, AssemblySyntax};
-pub use binary::{encode_instruction, Bytecode, is_double_word_instruction,
-                 try_decode_instruction, DoubleWord, Word};
-pub use instruction::{Instruction, Argument, Operation};
+pub use instruction::{Instruction, Argument, Operation, encode_instruction,
+                      Bytecode, is_double_word_instruction, try_decode_instruction,
+                      DoubleWord, Word};
 
-mod binary;
 mod instruction;
 mod assembly;
 
